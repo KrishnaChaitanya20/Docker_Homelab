ac8252bb132c3382222a2e77cc95e376dc2918a3
KrishnaChaitanya20 <krishnachaitanyadevunoori@gmail.com>
2025-09-07 21:28:31 +0530

    Enhance homelab.bat with start and stop commands for specific services

--- homelab.bat
+++ homelab.bat
@@ -2,12 +2,15 @@
 setlocal enabledelayedexpansion
 pushd "D:\Randoms\homelab"
 set COMMAND=%1
+set SERVICE=%2
 if "%COMMAND%" == "--help" (
     echo "Current working directory: %CD%"
-    echo "Usage: homelab.bat [all|up|down]"
+    echo "Usage: homelab.bat [all|up|down|start <service-name>|stop <service-name>]"
     echo "all: Start all services"
     echo "up: Start services one by one based on user input (Y/n)"
     echo "down: Stop all services"
+    echo "start <service-name>: Start a specific service by name"
+    echo "stop <service-name>: Stop a specific service by name"
 ) else if "%COMMAND%" == "all" (
     set COMMAND=up
     for /d %%i in (*) do (
@@ -17,10 +20,8 @@ if "%COMMAND%" == "--help" (
     )
 ) else if "%COMMAND%" == "up" (
     for /d %%i in (*) do (
-        @REM echo Starting %%i
         set /p "choice=Do you want to start %%i? [Y/n]: "
         if /i "!choice!" == "n" (
-            @REM echo Skipping %%i
             set "choice=Y"
         ) else (
             cd %%i
@@ -34,7 +35,31 @@ if "%COMMAND%" == "--help" (
         docker compose down
         cd ..
     )
+) else if "%COMMAND%" == "start" (
+    if not defined SERVICE (
+        echo "Error: Service name is required for start-service."
+    ) else (
+        if exist "%SERVICE%" (
+            cd "%SERVICE%"
+            docker compose up -d
+            cd ..
+        ) else (
+            echo "Error: Service '%SERVICE%' does not exist."
+        )
+    )
+) else if "%COMMAND%" == "stop" (
+    if not defined SERVICE (
+        echo "Error: Service name is required for stop-service."
+    ) else (
+        if exist "%SERVICE%" (
+            cd "%SERVICE%"
+            docker compose down
+            cd ..
+        ) else (
+            echo "Error: Service '%SERVICE%' does not exist."
+        )
+    )
 ) else (
-    echo "Usage: homelab.bat [all|up|down]"
+    echo "Usage: homelab.bat [all|up|down|start <service-name>|stop <service-name>]"
 )
 endlocal
